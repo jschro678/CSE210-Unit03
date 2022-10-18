ed42aa8084bac2244ff736d81bd7f68f9fd8ef0c
FIRST_NAME LAST_NAME <jschro678@gmail.com>
2022-10-18 16:28:44 -0600

    started jumper and word

--- Game/Jumper.cs
+++ Game/Jumper.cs
@@ -1,6 +1,54 @@
 using System;
+using System.Collections.Generic;
 
 namespace Unit03.Game
 {
-    public class Jumper { }
+    public class Jumper
+    {
+        public List<string> drawing = new List<string>
+        {
+            @" ---",
+            @"/___\",
+            @"\   /",
+            @" \ /",
+            @"  0",
+            @" /|\",
+            @" / \",
+            "       ",
+            "^^^^^^"
+        };
+        private int lives = 5;
+        private List<string> deadDrawing = new List<string>
+        {
+            @"^^^^^^",
+            @"  X",
+            @" /|\",
+            @" / \",
+        };
+        Word word = new Word();
+
+        private bool letterMatch = word.updateHint();
+
+        private Jumper() { }
+
+        private void updateJumper()
+        {
+            if (!letterMatch)
+            {
+                lives = lives - 1;
+                if (lives != 0)
+                {
+                    drawing.RemoveAt(0);
+                }
+                else
+                {
+                    drawing = deadDrawing;
+                }
+            }
+            else if (letterMatch)
+            {
+                return;
+            }
+        }
+    }
 }
