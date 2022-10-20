bbc32868edf08c597914355e28c885a6e436bb60
Pedro Ortiz <piqui0224@gmail.com>
2022-10-20 15:21:06 -0600

    Pedro's edits

---
+++ Game/Word.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+public class Word{
+
+    private void list.listOfWords(){
+        Random random = new Random();
+        private List<string> listOfWords = new List<string>  { //List of words to choose from go here. ("word1", "word2", etc.) };
+        int index = random.Next(wordDictionary.Count);
+        String randomWord = wordDictionary[index];
+        foreach (char x in randomWord)
+
+    }
+    private string Word()
+    {
+        Word word = new Word();
+    }
+
+    public void listletters(){
+        List<char> letters = new List<char>();
+        public List<char> guess = new List<char>();
+    }
+
+    private guessLetter(){
+        Console.WriteLine($"Guess a letter [a-z]: ")
+    }
+
+    private bool letterMatch = Word.updateHint();
+
+    private pickWord(){
+
+    }
+
+    private createHint(){
+
+    }
+}
