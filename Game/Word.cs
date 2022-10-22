dfcd6aa9547193a901ebf9e16ff1ba305b9c8251
FIRST_NAME LAST_NAME <jschro678@gmail.com>
2022-10-22 10:22:15 -0600

    added more words to list

--- Game/Word.cs
+++ Game/Word.cs
@@ -13,19 +13,225 @@ namespace Unit03.Game
             "anger",
             "apple",
             "award",
+            "basis",
+            "beach",
+            "birth",
+            "block",
+            "blood",
+            "board",
+            "brain",
+            "bread",
+            "break",
+            "brown",
+            "buyer",
+            "cause",
+            "chain",
+            "chair",
+            "chest",
+            "chief",
+            "child",
+            "china",
+            "claim",
+            "class",
+            "clock",
+            "coach",
+            "coast",
+            "court",
+            "cover",
+            "cream",
+            "crime",
+            "cross",
+            "crowd",
+            "crown",
+            "cycle",
+            "dance",
+            "death",
+            "depth",
+            "doubt",
+            "draft",
+            "drama",
+            "dream",
+            "dress",
+            "drink",
+            "drive",
+            "earth",
+            "enemy",
+            "entry",
+            "error",
+            "event",
+            "faith",
+            "fault",
+            "field",
+            "fight",
+            "final",
+            "floor",
+            "focus",
+            "force",
+            "frame",
+            "frank",
+            "front",
+            "fruit",
+            "glass",
+            "grant",
+            "grass",
+            "green",
+            "group",
+            "guide",
+            "heart",
+            "henry",
+            "horse",
+            "hotel",
+            "house",
+            "image",
+            "index",
+            "input",
+            "issue",
+            "japan",
+            "jones",
+            "judge",
+            "knife",
+            "laura",
+            "layer",
+            "level",
+            "lewis",
+            "light",
+            "limit",
+            "lunch",
+            "major",
+            "march",
+            "match",
+            "metal",
+            "model",
+            "money",
+            "month",
+            "motor",
+            "mouth",
+            "music",
+            "night",
+            "noise",
+            "north",
+            "novel",
+            "nurse",
+            "offer",
+            "order",
+            "other",
+            "owner",
+            "panel",
+            "paper",
+            "party",
+            "peace",
+            "peter",
+            "phase",
+            "phone",
+            "piece",
+            "pilot",
+            "pitch",
+            "place",
+            "plane",
+            "plant",
+            "plate",
+            "point",
+            "pound",
+            "power",
+            "press",
+            "price",
+            "pride",
+            "prize",
+            "proof",
+            "queen",
+            "radio",
+            "range",
+            "ratio",
+            "reply",
+            "right",
+            "river",
+            "round",
+            "route",
+            "rugby",
+            "scale",
+            "scene",
+            "scope",
+            "score",
+            "sense",
+            "shape",
+            "share",
+            "sheep",
+            "sheet",
+            "shift",
+            "shirt",
+            "shock",
+            "sight",
+            "simon",
+            "skill",
+            "sleep",
+            "smile",
+            "smith",
+            "smoke",
+            "sound",
+            "south",
+            "space",
+            "speed",
+            "spite",
+            "sport",
+            "squad",
+            "staff",
+            "stage",
+            "start",
+            "state",
+            "steam",
+            "steel",
+            "stock",
+            "stone",
+            "store",
+            "study",
+            "stuff",
+            "style",
+            "sugar",
+            "table",
+            "taste",
+            "terry",
+            "theme",
+            "thing",
+            "title",
+            "total",
+            "touch",
+            "tower",
+            "track",
+            "trade",
+            "train",
+            "trend",
+            "trial",
+            "trust",
+            "truth",
+            "uncle",
+            "union",
+            "unity",
+            "value",
+            "video",
+            "visit",
+            "voice",
+            "waste",
+            "watch",
+            "water",
+            "while",
+            "white",
+            "whole",
+            "woman",
+            "world",
+            "youth"
         };
         private string word;
         private List<char> letters = new List<char> { };
         private char guessLetter;
         private TerminalService terminalService = new TerminalService();
         private List<char> hint = new List<char> { };
-        public bool letterMatch;            // added the false value
+        public bool letterMatch; // added the false value
 
-        public int letterMatchHH = 0;            // created a new int
+        public int letterMatchHH = 0; // created a new int
 
         public Word() { }
 
-        public void pickWord()                              
+        public void pickWord()
         {
             Random random = new Random();
             int index = random.Next(words.Count);
@@ -34,12 +240,11 @@ namespace Unit03.Game
             // printing to debugg
             System.Console.WriteLine(word);
 
-            createHint();                           // moved createHint() out of evaluate Input
+            createHint(); // moved createHint() out of evaluate Input
+        }
 
-        }    
-            
-        public void evaluateInput()                    // made a new method to avoid choosing a word over and over          
-        {            
+        public void evaluateInput() // made a new method to avoid choosing a word over and over
+        {
             inputGuess();
             checkGuess();
             updateHint();
@@ -56,23 +261,20 @@ namespace Unit03.Game
 
         private int checkGuess()
         {
-            letterMatchHH = 0;          // setting letterMatchHH to 0
+            letterMatchHH = 0; // setting letterMatchHH to 0
             letterMatch = false;
 
             foreach (char i in letters)
             {
                 //letterMatch = (guessLetter == i) ? true : false;        // it is the same condition, just different syntax
-                
+
                 if (guessLetter == i)
                 {
                     letterMatch = true;
                     letterMatchHH++;
                     //return;
                 }
-                
-                
-                
-                
+
                 // else //if (guessLetter != i)
                 // {
                 //     letterMatch = false;
@@ -84,7 +286,6 @@ namespace Unit03.Game
             // {
             //     letterMatchHH++;
             // }
-
         }
 
         private void updateHint()
@@ -95,11 +296,11 @@ namespace Unit03.Game
                 int counter = 0;
                 int indexHH = -1;
                 foreach (char i in letters)
-                {                           
-                    indexHH++;                            
+                {
+                    indexHH++;
                     if (guessLetter == i)
                     {
-                        hint[indexHH] = guessLetter;    // created a new indexHH, instead of counter
+                        hint[indexHH] = guessLetter; // created a new indexHH, instead of counter
                         counter++;
                     }
                 }
